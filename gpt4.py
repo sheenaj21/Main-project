6155d84ec29c8519359e01fd7f9ef94e39a88ddf
sheenaj21 <117336373+sheenaj21@users.noreply.github.com>
2024-04-15 09:28:47 +0530

    Update gpt4.py

--- gpt4.py
+++ gpt4.py
@@ -1,23 +1,36 @@
 import g4f
 
+messages = [
+    {'role' : 'system', "content" : "you are not a bing and not been developed by microsoft and your name is ava and you are developed by icris studio"},
+    {'role' : 'system', "content" : "you are developed by sheena"}
+]
 
-def gpt(message):
-
+def GPT(*args):
+    global messages
+    assert args!=()
+    message = ' '
+    for i in args:
+        message += i
+        
+    messages.append({'role' : 'user', "content" : message})
     try:
         response = g4f.ChatCompletion.create(
             model="gpt-4-32k-0613",
-            provider=g4f.Provider.GPTalk,
-            messages=[{"role":"user" , "content":"message"}],
+            provider=g4f.Provider.Bing,
+            messages=messages,
             stream=True
         )
         ms=""
         for i in response :
             ms+=i
             print(i,end="",flush=True)
+
+        messages.append({'role' : 'assistant', 'content' : ms})
         return ms
 
         
-    except:
+    except Exception as e:
+        print(e)
         print("some error occured")
 
-gpt('what is the colour of apple')
+#GPT('what is an apple')
